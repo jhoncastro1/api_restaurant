dd60bb2d056574632fcbedda7b11a44dc20f9223
jhoncastro1 <jhoncastro2004eg@gmail.com>
2024-02-20 09:35:41 -0500

    Creation of the spring boot structure

---
+++ src/main/java/com/jhon/api_restaurant/ApiRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.jhon.api_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestaurantApplication.class, args);
+    }
+
+}
